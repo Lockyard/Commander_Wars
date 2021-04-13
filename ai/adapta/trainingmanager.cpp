7bfc67c176ef6e0881c9864ad73754882e842efe
Lorenzo <hyperl@hotmail.it>
2021-04-13 18:46:26 +0200

    added save file support for training

--- ai/adapta/trainingmanager.cpp
+++ ai/adapta/trainingmanager.cpp
@@ -1,25 +1,141 @@
 #include "trainingmanager.h"
 #include "coreengine/console.h"
-#include <iostream>
+#include <QSettings>
+#include <QJsonDocument>
+#include <QJsonArray>
+#include <QJsonObject>
 #include "ai/genetic/evofunctions.h"
 
-TrainingManager::TrainingManager(QObject *parent) : QObject(parent), m_isEvoManagerInitialized(false) {
-    std::cout << "training manager was created";
-}
+TrainingManager::TrainingManager(QObject *parent) : QObject(parent), m_currGenNumberCount(0), m_totalGenCount(0),
+    m_continueTraining(true), m_isEvoManagerInitialized(false)
+    {}
 
 bool TrainingManager::s_isInstanceInitialized = false;
 
 TrainingManager TrainingManager::s_instance(nullptr);
 
 
-void TrainingManager::readIni() {
-    //todo read from file, this is a mockup
-    m_currWVIndex = 0;
-    m_currWVMatchNumber = 0;
-    m_matchNumberTarget = 3;
-    m_elitismDegree = 1;
-    m_randomismDegree = 1;
+void TrainingManager::loadIni(QString filename) {
+    if (QFile::exists(filename))
+    {
+        m_iniFileName = filename;
+        QSettings settings(filename, QSettings::IniFormat);
+
+        //load stuff for the training manager
+        settings.beginGroup("TrainingInfo");
+        bool ok = false;
+        m_matchNumberTarget = settings.value("MatchNumberTarget", 10).toInt(&ok);
+        if(!ok)
+            m_matchNumberTarget = 10;
+        m_populationFileName = settings.value("PopulationFileName", "resources/aidata/adapta/population.json").toString();
+        m_maxGenerationCount = settings.value("MaxGenerationCount", 1000).toInt(&ok);
+        if(!ok)
+            m_maxGenerationCount = 1000;
+        m_saveOnNewBestFit = settings.value("SaveOnNewBestFit", true).toBool();
+        m_genNumberTargetSave = settings.value("GenerationNumberTargetSave", -1).toInt(&ok);
+        if(!ok)
+            m_genNumberTargetSave = 10;
+        settings.endGroup();
+
+        settings.beginGroup("CurrentState");
+        m_currWVIndex = settings.value("CurrWVIndex", 0).toInt(&ok);
+        if(!ok)
+            m_currWVIndex = 0;
+        m_currWVMatchNumber = settings.value("CurrWVMatchNumber", 0).toInt(&ok);
+        if(!ok)
+            m_currWVMatchNumber = 0;
+        m_currBestSavedFitness = static_cast<float>(settings.value("CurrBestSavedFitness", 0.0f).toDouble(&ok));
+        if(!ok)
+            m_currBestSavedFitness = 0.0f;
+        settings.endGroup();
+
+        //load stuff for the evolution manager
+        settings.beginGroup("EvolutionManager");
+        qint32 populationSize, weightVectorLength, elitismDegree, randomismDegree, generation;
+        float minWeight, maxWeight;
+        populationSize = settings.value("PopulationSize", 10).toInt(&ok);
+        if(!ok)
+            populationSize = 10;
+        weightVectorLength = settings.value("WeightVectorLength", 10).toInt(&ok);
+        if(!ok)
+            weightVectorLength = 10;
+        elitismDegree = settings.value("ElitismDegree", 2).toInt(&ok);
+        if(!ok)
+            elitismDegree = 2;
+        randomismDegree = settings.value("RandomismDegree", 1).toInt(&ok);
+        if(!ok)
+            randomismDegree = 1;
+        generation = settings.value("Generation", 0).toInt(&ok);
+        if(!ok)
+            generation = 0;
+        minWeight = static_cast<float>(settings.value("MinWeight", -10.0).toDouble(&ok));
+        if(!ok)
+            minWeight = -10.0f;
+        maxWeight = static_cast<float>(settings.value("MaxWeight", 10.0).toDouble(&ok));
+        if(!ok)
+            maxWeight = 10.0f;
+        settings.endGroup();
+
+        m_evolutionManager.initialize(populationSize, weightVectorLength, minWeight, maxWeight, elitismDegree, randomismDegree);
+        m_evolutionManager.setGeneration(generation);
+
+    } else {
+        Console::print("Training manager couldn't load ini file (" + filename + ")! Default values applied", Console::eWARNING);
+        m_matchNumberTarget = 10;
+        m_populationFileName = "resources/aidata/adapta/population.json";
+        m_currWVIndex = 0;
+        m_currWVMatchNumber = 0;
+
+        m_evolutionManager.initialize(10, 10, -10.0f, 10.0f, 2, 1);
+        m_evolutionManager.setGeneration(0);
+    }
 }
+
+
+void TrainingManager::saveState(QString iniFilename) {
+    Console::print("Training Manager is saving...", Console::eDEBUG);
+    QSettings settings(iniFilename, QSettings::IniFormat);
+    bool ok = false;
+
+    //load backup infos
+    settings.beginGroup("SaveInfo");
+    qint32 saveIndex, maxSaveIndex;
+    QString saveNamePrefix;
+    QString saveNameExtension;
+    saveIndex = settings.value("NextSaveIndex", 0).toInt(&ok);
+    if(!ok)
+        saveIndex = 0;
+    maxSaveIndex = settings.value("MaxSaveIndex", 10).toInt(&ok);
+    if(!ok)
+        maxSaveIndex = 10;
+    saveNamePrefix = settings.value("SaveNamePrefix", "resources/aidata/adapta/pop_record_").toString();
+    saveNameExtension = settings.value("SaveNameExtension", ".json").toString();
+
+    //save a population file in the correct backup file
+    m_populationFileName = saveNamePrefix + QString::number(saveIndex) + saveNameExtension;
+    m_evolutionManager.savePopulationToJsonFile(m_populationFileName);
+    settings.setValue("NextSaveIndex", saveIndex+1);
+    settings.endGroup();
+
+    //update the file name to be load to the last one
+    settings.beginGroup("TrainingInfo");
+    settings.setValue("PopulationFileName", m_populationFileName);
+    settings.endGroup();
+
+    //save the state
+    settings.beginGroup("CurrentState");
+    settings.setValue("CurrWVIndex", m_currWVIndex);
+    settings.setValue("CurrWVMatchNumber", m_currWVMatchNumber);
+    settings.setValue("CurrBestSavedFitness", m_currBestSavedFitness);
+    settings.endGroup();
+
+    //save the ini file. doc says it not really necessary but just in case
+    settings.sync();
+
+    Console::print("Training Manager: save complete.", Console::eDEBUG);
+}
+
+
 
 
 void TrainingManager::setupForMatch() {
@@ -36,6 +152,21 @@ WeightVector TrainingManager::assignWeightVector() {
     return m_evolutionManager.getPopulation()[m_currWVIndex];
 }
 
+//relies on the automation of the init.js file (in templates, in install is in the base folder)
+void TrainingManager::stopTraining() {
+    m_continueTraining = false;
+    Interpreter* pInterpreter = Interpreter::getInstance();
+    QString object = "Init";
+    QString func = "stopTraining";
+    if (pInterpreter->exists(object, func))
+    {
+        QJSValueList args;
+        QJSValue value = pInterpreter->newQObject(this);
+        args << value;
+        pInterpreter->doFunction(object, func, args);
+    }
+}
+
 
 //slots
 void TrainingManager::onVictory() {
@@ -48,16 +179,23 @@ void TrainingManager::onVictory() {
 
 //private fncts
 void TrainingManager::initializeEvolutionManager() {
-    readIni();
-    m_evolutionManager = EvolutionManager(10, 3, -10.0f, 10.0f, evoenums::mixRandom, .05f);
+    Interpreter::setCppOwnerShip(this);
+    Mainapp* pApp = Mainapp::getInstance();
+    this->moveToThread(pApp->getWorkerthread());
+
+    //load ini also set some stuff for the evolution manager
+    loadIni("resources/aidata/adapta/training.ini");
+    bool loadOk = m_evolutionManager.loadPopulationFromJsonFile(m_populationFileName);
+    if(!loadOk) {
+        Console::print("Creating random population", Console::eDEBUG);
+        m_evolutionManager.createRandomPopulation();
+    }
     m_evolutionManager.setMutationFunction(evofunc::individualRandomMutation);
-    m_evolutionManager.setElitismDegree(m_elitismDegree);
-    m_evolutionManager.setRandomismDegree(m_randomismDegree);
-    m_evolutionManager.maximizeFitness();
-    m_evolutionManager.createRandomPopulation();
 
     m_isEvoManagerInitialized = true;
 }
+
+
 
 void TrainingManager::advanceMatchCount() {
     //increase counter of match to select the correct weight to be tested
@@ -71,12 +209,20 @@ void TrainingManager::advanceMatchCount() {
         m_currWVIndex++;
         //if there are no new WVs to test for this generation then evolve population
         if(m_currWVIndex == m_evolutionManager.getPopulationSize()) {
-            m_evolutionManager.performOneEvolutionStep();
-            Console::print("Training manager evolved population (gen " +
-                           QString::number(m_evolutionManager.getGeneration()) + "):\n" +
-                           m_evolutionManager.toQStringPopulation(), Console::eDEBUG);
-            //the first new WV to be examined is the first after the elite vectors, which were already tested
-            m_currWVIndex = m_elitismDegree;
+
+            //population are (eventually) saved BEFORE evolving, so here all vectors are evalued and sorted
+            m_evolutionManager.sortPopulationByFitness();
+            saveIfProgress();
+
+            //now do evolution if training goes on
+            if(m_continueTraining) {
+                m_evolutionManager.performOneEvolutionStep();
+                Console::print("Training manager evolved population (gen " +
+                               QString::number(m_evolutionManager.getGeneration()) + "):\n" +
+                               m_evolutionManager.toQStringPopulation(), Console::eDEBUG);
+                //the first new WV to be examined is the first after the elite vectors, which were already tested
+                m_currWVIndex = m_evolutionManager.getElitismDegree();
+            }
         }
     }
 }
@@ -91,4 +237,25 @@ void TrainingManager::evaluateFitnessOfCurrentWV() {
                    QString::number(m_evolutionManager.getPopulationSize()) +
                    " got fitness of " + QString::number(dummyFitness) + "\n" +
                    m_evolutionManager.getPopulation()[m_currWVIndex].toQString(), Console::eDEBUG);
+}
+
+
+void TrainingManager::saveIfProgress() {
+    m_currGenNumberCount++;
+    m_totalGenCount++;
+    //if we reached the target generations of evolutions, save and stop training
+    if(m_totalGenCount >= m_maxGenerationCount) {
+        saveState(m_iniFileName);
+        stopTraining();
+    }
+    //else save if there's a new best fitness (if option enabled)
+    else if(m_saveOnNewBestFit && (m_evolutionManager.getNthBestWeightVector(0).getFitness() > m_currBestSavedFitness)) {
+        m_currBestSavedFitness = m_evolutionManager.getNthBestWeightVector(0).getFitness();
+        m_currGenNumberCount = 0;
+        saveState(m_iniFileName);
+        //else save if the defined number of generations has passed (if option enabled)
+    } else if(m_genNumberTargetSave > 0 && m_currGenNumberCount >= m_genNumberTargetSave) {
+        m_currGenNumberCount = 0;
+        saveState(m_iniFileName);
+    }
 }
--- ai/adapta/trainingmanager.h
+++ ai/adapta/trainingmanager.h
@@ -17,9 +17,18 @@ public:
         return s_instance;
     }
 
-    void readIni();
+    void loadIni(QString filename);
+    /**
+     * @brief saveState create a save of the current population. Info of backup are read on a ini file specified.
+     * Also update the state that will be read from the ini file
+     * @param iniFilename
+     */
+    void saveState(QString iniFilename);
+
     void setupForMatch();
     WeightVector assignWeightVector();
+
+    void stopTraining();
 
 public slots:
     void onVictory();
@@ -28,11 +37,28 @@ private:
     explicit TrainingManager(QObject *parent = nullptr);
     EvolutionManager m_evolutionManager;
 
+    bool m_continueTraining;
+
+    QString m_iniFileName;
+    QString m_populationFileName;
+
+    //params to know when to save
+    //save population each time a new best fit vector appears?
+    bool m_saveOnNewBestFit;
+    float m_currBestSavedFitness;
+    //save population each N generations have passed since the last save (which can occur before if the previous bool is set to true)
+    //<= 0 to never save automatically after N generations
+    qint32 m_genNumberTargetSave;
+    qint32 m_currGenNumberCount;
+
+
+    //counters for matches and vectors, to know which vector at each match should be used and when it's going to be evaluated
     qint32 m_currWVIndex;
     qint32 m_currWVMatchNumber;
     qint32 m_matchNumberTarget;
-    qint32 m_elitismDegree;
-    qint32 m_randomismDegree;
+    //how many gen at most the training must do. If negative, go infintely
+    qint32 m_maxGenerationCount;
+    qint32 m_totalGenCount;
 
     bool m_isEvoManagerInitialized;
 
@@ -43,9 +69,7 @@ private:
     void advanceMatchCount();
     void evaluateFitnessOfCurrentWV();
 
-
-signals:
-
+    void saveIfProgress();
 };
 
 #endif // TRAININGMANAGER_H
